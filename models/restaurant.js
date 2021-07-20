2bb2d5c0b6dcc5847275893f2298959e744cbae9
Zeru <zeruna@ad.vzwcorp.com>
2021-07-19 22:08:36 -0500

    tried as best as i could

---
+++ models/restaurant.js
@@ -0,0 +1,15 @@
+const db = require('../db');
+const { DataTypes, Model } = require('sequelize');
+
+
+class Restaurant extends Model {}
+
+Restaurant.init({
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+}, {
+    sequelize: db,
+    timestamps: false,
+});
+
+module.exports = Restaurant;
